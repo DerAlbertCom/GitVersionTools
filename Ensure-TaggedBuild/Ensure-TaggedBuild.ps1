0072b4a5002756c6fa62fffbc5cee8f7bcead611
Albert Weinert <a.weinert@pyton.travel>
2017-02-18 14:22:02 +0100

    fixed not using patchlevel, going to commits since version source

--- Ensure-TaggedBuild/Ensure-TaggedBuild.ps1
+++ Ensure-TaggedBuild/Ensure-TaggedBuild.ps1
@@ -5,7 +5,7 @@ param(
 )
 
 $currentBranch = $Env:BUILD_SOURCEBRANCHNAME
-$patchLevel = $env:GitVersion_Patch
+$commits = $env:GitVersion_CommitsSinceVersionSource
 $currentVersion = $env:GITVERSION_MajorMinorPatch
 
 if (-not $currentBranch )  {
@@ -13,19 +13,19 @@ if (-not $currentBranch )  {
     Exit 1
 }
 
-if (-not $patchLevel) {
+if (-not $commits) {
     Write-Error "You have to start the GitVersion Task before running that script"
     Exit 1
 }
 
-Write-Host "Ensure:Tagged current branch os $currentBranch, the pathlevel is $patchLevel, the release branch is set to $masterBranch"
+Write-Host "Ensure:Tagged current branch os $currentBranch, they are $commits after version source, the release branch is set to $masterBranch"
 
-if ($patchLevel -eq "0" -And $currentBranch -ne $masterBranch ) {
-    Write-Error "You can't build $currentBranch with a patchLevel of $patchLevel"
+if ($commits -eq "0" -And $currentBranch -ne $masterBranch ) {
+    Write-Error "You can't build $currentBranch with commits count of $commits"
     Exit 1
 }
-if ($patchLevel -ne "0" -And $currentBranch -eq $masterBranch ) {
-    Write-Error "You can't build $currentBranch with a patchLevel of $patchLevel, a release build must have a direct tag"
+if ($commits -ne "0" -And $currentBranch -eq $masterBranch ) {
+    Write-Error "You can't build $currentBranch with a commits count of $commits, a release build must have a direct version tag"
     Write-Error "You must a least tag with a higher version that $currentVersion"
     Write-Error "git tag NEWVERSION"
     Write-Error "git push origin $currentBranch --tags"
