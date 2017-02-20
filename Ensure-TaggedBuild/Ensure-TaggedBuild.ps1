a57ade43afdf50fc727e9b691a3280c3c41331ef
Albert Weinert <a.weinert@pyton.travel>
2017-02-20 10:55:10 +0100

    fixed some types, set visibility to public

--- Ensure-TaggedBuild/Ensure-TaggedBuild.ps1
+++ Ensure-TaggedBuild/Ensure-TaggedBuild.ps1
@@ -14,16 +14,17 @@ if (-not $currentBranch )  {
 }
 
 if (-not $commits) {
-    Write-Error "You have to start the GitVersion Task before running that script"
+    Write-Error "You have to run the GitVersion Task before running this task"
     Exit 1
 }
 
-Write-Host "Ensure:Tagged current branch os $currentBranch, they are $commits after version source, the release branch is set to $masterBranch"
+Write-Host "Ensure:Tagged current branch is $currentBranch, they are $commits commits after version source, the release branch is set to $masterBranch"
 
 if ($commits -eq "0" -And $currentBranch -ne $masterBranch ) {
     Write-Error "You can't build $currentBranch with commits count of $commits"
     Exit 1
 }
+
 if ($commits -ne "0" -And $currentBranch -eq $masterBranch ) {
     Write-Error "You can't build $currentBranch with a commits count of $commits, a release build must have a direct version tag"
     Write-Error "You must a least tag with a higher version that $currentVersion"
